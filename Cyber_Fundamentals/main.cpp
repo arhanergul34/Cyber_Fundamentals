582ab50a79f214059bc27e02274b51993a239270
arhanergul <arhanergul34@gmail.com>
2026-06-16 16:01:28 +0200

    Add project files.

---
+++ Cyber_Fundamentals/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file main.cpp
+ * @brief Cyber Security Oriented C++ Journey - Phase 1: Memory Anatomy
+ * @details This project analyzes variable allocation on the Stack, byte sizes,
+ * and hexadecimal physical memory addresses for cyber security fundamentals.
+ */
+
+#include <iostream>
+
+int main() {
+	int cyber_code = 50;
+	char log_character = 'A';
+
+	std::cout << "== VARIABLE MEMORY SIZES (BYTES) ===" << std::endl;
+	std::cout << "Size of cyber_code: " << sizeof(cyber_code) << "bytes." << std::endl;
+	std::cout << "Size of log_character: " << sizeof(log_character) << " byte." << std::endl;
+
+	std::cout << "--------------------------------------------------" << std::endl;
+	std::cout << "=== PHYSICAL MEMORY ADDRESSES (HEXADECIMAL) ===" << std::endl;
+
+	std::cout << "RAM Address of cyber_code : " << &cyber_code << std::endl;
+	std::cout << "RAM Address of log_character : " << (void*)&log_character << std::endl;
+
+	return 0;
+
+}
